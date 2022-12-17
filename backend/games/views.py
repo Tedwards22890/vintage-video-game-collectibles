9cabe110751886f97882ed5db42526cb7384b828
Travis Edwards <tedwards22890@gmail.com>
2022-12-17 13:37:51 -0500

    Further updated games views to include deletion.

--- backend/games/views.py
+++ backend/games/views.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
@@ -16,11 +17,12 @@ def game_list(request):
     return Response(serializer.data)
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'DELETE'])
 @permission_classes([IsAuthenticated])
 def user_games(request):
     print(
         'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    game= get_object_or_404(Game, pk=pk)
     if request.method == 'POST':
         serializer = GamesSerializer(data=request.data)
         if serializer.is_valid():
@@ -31,10 +33,14 @@ def user_games(request):
         games = Game.objects.filter(user_id=request.user.id)
         serializer = GamesSerializer(games, many=True)
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        game.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def games_by_id(request, game_id):
-    games =Game.objects.filter(game_id=request.game.id)
-    serializer = GamesSerializer(games, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        games =Game.objects.filter(game_id=request.game.id)
+        serializer = GamesSerializer(games, many=True)
+        return Response(serializer.data)
